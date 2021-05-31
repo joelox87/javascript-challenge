6396eb772a70a0324ce2bb0975de30a5a7681473
joelox87 <j.aponte87@gmail.com>
2021-05-31 12:21:57 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,9 @@
-// from data.js
 var tableData = data;
 
-// Use D3 to select the table body
 var tbody = d3.select("tbody");
-// Use D3 to select the table
-var table = d3.select("table");
-//remove any children from the table to 
+var table = d3.select("table"); 
 tbody.html("");
-// Use D3 to set the table class to `table table-striped`
 table.attr("class", "table table-striped");
-// Use d3 to append 1 cell per UFO sighting value 
 tableData.forEach(function(ufoSightEntry){
     console.log(ufoSightEntry);
     var row =tbody.append("tr");
@@ -22,27 +16,20 @@ tableData.forEach(function(ufoSightEntry){
 // Select the button
 var button = d3.select("#filter-btn");
 button.on("click", function() {
-  // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-  // Get the value property of the input element
   var inputValue = inputElement.property("value");
   console.log(inputValue);
   console.log(tableData);
   var filteredData = tableData.filter(ufoSightEntry => (ufoSightEntry.datetime ===inputValue));
   console.log(filteredData);
 
-// Use D3 to select the table body
 var tbody = d3.select("tbody");
-// Use D3 to select the table
 var table = d3.select("table");
-//remove any children from the table to 
 tbody.html("");
-// Use D3 to set the table class to `table table-striped`
 table.attr("class", "table table-striped");
 
 
 if (inputValue!=="") {
-// Use d3 to append 1 cell per UFO sighting value 
 filteredData.forEach(function(ufoSightEntry){
     console.log(ufoSightEntry);
     var row =tbody.append("tr");
